485fd999df6d84c4fb9eee7026d50ed1b46496c3
ecrypa <>
2019-10-28 20:55:00 +0100

    implement min_element v22

--- include/metal/list.hpp
+++ include/metal/list.hpp
@@ -38,6 +38,7 @@
 #include "list/min_element.v17.ind.find.hpp"
 #include "list/min_element.v18.ind.transpose.hpp"
 #include "list/min_element.v19.ind.transpose.hpp"
+#include "list/min_element.v22.hpp"
 #include "list/min_element.v4.homf.hpp"
 #include "list/min_element.v5.alias_recursion.hpp"
 #include "list/min_element.v6.alias_recursion_grouped.hpp"
